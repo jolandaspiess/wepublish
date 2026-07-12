ed69e5befa7414027022d40a0040664ae1d05fcf
dsheyp <dsheyp@gmail.com>
2026-07-12 21:54:35 +0200

    add default payment periodicity

--- libs/peering/api/src/lib/remote/graphql.ts
+++ libs/peering/api/src/lib/remote/graphql.ts
@@ -2199,6 +2199,7 @@ export type MemberPlan = HasImage & {
   confirmationPage?: Maybe<Page>;
   confirmationPageId?: Maybe<Scalars['String']>;
   currency: Currency;
+  defaultPaymentPeriodicity?: Maybe<PaymentPeriodicity>;
   description?: Maybe<Scalars['RichText']>;
   extendable: Scalars['Boolean'];
   externalReward?: Maybe<Scalars['String']>;
@@ -2806,6 +2807,7 @@ export type MutationCreateMemberPlanArgs = {
   availablePaymentMethods: Array<AvailablePaymentMethodInput>;
   confirmationPageId?: InputMaybe<Scalars['String']>;
   currency: Currency;
+  defaultPaymentPeriodicity?: InputMaybe<PaymentPeriodicity>;
   description?: InputMaybe<Scalars['RichText']>;
   extendable: Scalars['Boolean'];
   externalReward?: InputMaybe<Scalars['String']>;
@@ -3676,6 +3678,7 @@ export type MutationUpdateMemberPlanArgs = {
   availablePaymentMethods?: InputMaybe<Array<AvailablePaymentMethodInput>>;
   confirmationPageId?: InputMaybe<Scalars['String']>;
   currency?: InputMaybe<Currency>;
+  defaultPaymentPeriodicity?: InputMaybe<PaymentPeriodicity>;
   description?: InputMaybe<Scalars['RichText']>;
   extendable?: InputMaybe<Scalars['Boolean']>;
   externalReward?: InputMaybe<Scalars['String']>;
@@ -4658,15 +4661,17 @@ export type PeriodicJob = {
 export type PeriodicityPrice = {
   __typename?: 'PeriodicityPrice';
   amountMax?: Maybe<Scalars['Int']>;
-  amountMin: Scalars['Int'];
+  amountMin?: Maybe<Scalars['Int']>;
   amountTarget?: Maybe<Scalars['Int']>;
+  label?: Maybe<Scalars['String']>;
   periodicity: PaymentPeriodicity;
 };
 
 export type PeriodicityPriceInput = {
   amountMax?: InputMaybe<Scalars['Int']>;
-  amountMin: Scalars['Int'];
+  amountMin?: InputMaybe<Scalars['Int']>;
   amountTarget?: InputMaybe<Scalars['Int']>;
+  label?: InputMaybe<Scalars['String']>;
   periodicity: PaymentPeriodicity;
 };
 
@@ -6337,6 +6342,7 @@ export type SubscribeBlock = BaseBlock & {
   memberPlanIds: Array<Scalars['String']>;
   memberPlanRenderSettings: Array<SubscribeBlockMemberPlanRenderSetting>;
   memberPlans: Array<MemberPlan>;
+  periodicityDisplay?: Maybe<SubscribePeriodicityDisplay>;
   showDiscountCodes: Scalars['Boolean'];
   showGoodies: Scalars['Boolean'];
   type: BlockType;
@@ -6360,6 +6366,7 @@ export type SubscribeBlockInput = {
   hideRepeatGoodieOnUpgrade?: Scalars['Boolean'];
   memberPlanIds?: Array<Scalars['String']>;
   memberPlanRenderSettings: Array<SubscribeBlockMemberPlanRenderSettingInput>;
+  periodicityDisplay?: InputMaybe<SubscribePeriodicityDisplay>;
   showDiscountCodes?: Scalars['Boolean'];
   showGoodies?: Scalars['Boolean'];
 };
@@ -6409,6 +6416,11 @@ export enum SubscribeBlockRenderLayout {
   None = 'None',
   Picker = 'Picker',
   Slider = 'Slider'
+}
+
+export enum SubscribePeriodicityDisplay {
+  Dropdown = 'Dropdown',
+  OfferCards = 'OfferCards'
 }
 
 export type SubscriptionCreatedAction = BaseAction & HasSubscriptionLc & {
